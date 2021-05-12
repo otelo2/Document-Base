f85f70db6f849cadc25e21c2e110e98f36858b93
Otelo2 <josean2009@live.com.mx>
2021-05-12 18:58:51 -0500

    Sync me up

--- src/fastRun.bat
+++ src/fastRun.bat
@@ -1,4 +1,7 @@
 javac org/tartarus/snowball/SnowballProgram.java
 javac org/tartarus/snowball/ext/englishStemmer.java
 javac org/tartarus/snowball/TestApp.java
-java -classpath . org/tartarus/snowball/TestApp english test.txt -o output.txt
+Rem esto es un comentatio: java -classpath . org/tartarus/snowball/TestApp english "test.txt" -o output.txt
+java -classpath . org/tartarus/snowball/TestApp english 1.txt -o output.txt
+java -classpath . org/tartarus/snowball/TestApp english 2.txt -o output.txt
+java -classpath . org/tartarus/snowball/TestApp english 3.txt -o output.txt
