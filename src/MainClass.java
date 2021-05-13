8a438f67f76dc19f30452ca35c82fadc4f57a0c2
Otelo2 <josean2009@live.com.mx>
2021-05-12 21:20:00 -0500

    Perform the freqT function on all files

--- src/MainClass.java
+++ src/MainClass.java
@@ -42,6 +42,13 @@ public class MainClass {
         TermFrequency.calculateFrequency("1");
         TermFrequency.calculateFrequency("2");
         TermFrequency.calculateFrequency("3");
+        /*TermFrequency.calculateFrequency("4");
+        TermFrequency.calculateFrequency("5");
+        TermFrequency.calculateFrequency("6");
+        TermFrequency.calculateFrequency("7");
+        TermFrequency.calculateFrequency("8");
+        TermFrequency.calculateFrequency("9");
+        TermFrequency.calculateFrequency("10");*/
 
         //3. Matrix creation
         
